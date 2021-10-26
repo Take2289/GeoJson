c2ac5ce50d93e09dc9cd623d8be44db832ea818a
Ty Davidson <62067132+Take2289@users.noreply.github.com>
2021-10-26 16:44:42 -0400

    Update Conv_to_shp.py

--- Conv_to_shp.py
+++ Conv_to_shp.py
@@ -1,18 +1,30 @@
 import json 
+from matplotlib import pyplot as plt
+import requests 
 
-import requests
-
-req = requests.get("https://maps2.dcgis.dc.gov/dcgis/rest/services/FEEDS/MPD/MapServer/35/query?where=1%3D1&outFields=CCN,METHOD,BLOCK,DISTRICT,CENSUS_TRACT,LATITUDE,LONGITUDE,START_DATE,END_DATE,REPORT_DAT&outSR=4326&f=json")
+req = requests.get ("https://maps2.dcgis.dc.gov/dcgis/rest/services/FEEDS/MPD/MapServer/35/query?where=1%3D1&outFields=BLOCK,LATITUDE,LONGITUDE,START_DATE,END_DATE,REPORT_DAT&outSR=4326&f=json")
 Geodata_json = req.json()
 
-Geodata_str = json.dumps(Geodata_json, indent=2)
+d = json.dumps(Geodata_json, indent=2)
 
-f = open ('Geo.shp','w')
-f.write(Geodata_str)
+order = [1,2,3,4,5,6,7,8,9,10]
+info = [10,20,30,40,50,60,70,80,90,100]
+
+i = plt.plot(info, order) 
+plt.show()
+
+s = str(i) 
+
+x = (s + d)
+
+
+f = open('Geo.shp', 'w')
+f.write(x)
 f.close()
 
+Shape =(x)
 
-Shape = (f,Geodata_str)
 
-print(Shape)
+print(Shape) 
+
 
