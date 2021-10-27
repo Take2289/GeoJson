bdf021d23127d10d0fe948b55208a6766593cb58
Ty Davidson <62067132+Take2289@users.noreply.github.com>
2021-10-27 14:47:59 -0400

    Update Conv_to_shp.py

--- Conv_to_shp.py
+++ Conv_to_shp.py
@@ -1,11 +1,11 @@
-import json 
+import geojson 
 from matplotlib import pyplot as plt
 import requests 
 
 req = requests.get ("https://maps2.dcgis.dc.gov/dcgis/rest/services/FEEDS/MPD/MapServer/35/query?where=1%3D1&outFields=BLOCK,LATITUDE,LONGITUDE,START_DATE,END_DATE,REPORT_DAT&outSR=4326&f=json")
 Geodata_json = req.json()
 
-d = json.dumps(Geodata_json, indent=2)
+d = geojson.dumps(Geodata_json, indent=2)
 
 order = [1,2,3,4,5,6,7,8,9,10]
 info = [10,20,30,40,50,60,70,80,90,100]
